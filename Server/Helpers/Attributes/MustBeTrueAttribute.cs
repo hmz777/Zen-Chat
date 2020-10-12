f8bef15c2e5c77f9499a796fa8583af037cd12d9
HMZ <hamzi1995@gmail.com>
2020-10-13 02:47:55 +0300

    Create MustBeTrueAttribute.cs

---
+++ Server/Helpers/Attributes/MustBeTrueAttribute.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MDProject.Helpers.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class MustBeTrueAttribute : ValidationAttribute, IClientModelValidator
+    {
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            MergeAttribute(context.Attributes, "data-val", "true");
+            var errorMsg = FormatErrorMessage(context.ModelMetadata.GetDisplayName());
+            MergeAttribute(context.Attributes, "data-val-mustbetrue", errorMsg);
+        }
+
+        public override bool IsValid(object value)
+        {
+            return value != null && (bool)value == true;
+        }
+
+        private bool MergeAttribute(
+                  IDictionary<string, string> attributes,
+                  string key,
+                  string value)
+        {
+            if (attributes.ContainsKey(key))
+            {
+                return false;
+            }
+            attributes.Add(key, value);
+            return true;
+        }
+
+    }
+}
