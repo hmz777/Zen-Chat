bfe2c0f37b4cda5617a90d448f161c1f7126b5ce
HMZ <hamzi1995@gmail.com>
2020-10-13 02:48:08 +0300

    Create Gender.cs

---
+++ Server/Models/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCBlazorChatApp.Server.Models
+{
+    public enum Gender : int
+    {
+        Male = 1,
+        Female = 2
+    }
+}
