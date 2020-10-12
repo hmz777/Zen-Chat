ac9bc3648ccccc251c1d4514e35b860111ae6784
HMZ <hamzi1995@gmail.com>
2020-10-13 02:47:41 +0300

    Update registration and login logic

--- Client/wwwroot/js/main.js
+++ Client/wwwroot/js/main.js
@@ -6,4 +6,14 @@
             target.parents(".input-wrapper--text").eq(0).addClass("focused");
         }
     });
+
+    $.validator.addMethod("mustbetrue",
+        function (value, element, parameters) {
+            return element.checked;
+        });
+
+    $.validator.unobtrusive.adapters.add("mustbetrue", [], function (options) {
+        options.rules.mustbetrue = {};
+        options.messages["mustbetrue"] = options.message;
+    });
 });
--- Server/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Server/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,16 +1,50 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using MDProject.Helpers.Attributes;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MVCBlazorChatApp.Server.Models;
 
 namespace MVCBlazorChatApp.Server.Areas.Identity.Pages.Account
 {
     public class RegisterModel : PageModel
     {
+        public RegisterDTO RegisterDTO { get; set; }
+
         public void OnGet()
         {
         }
+
+        public void OnPost()
+        {
+        }
+    }
+
+    public class RegisterDTO
+    {
+        [Required(ErrorMessage = "This field is required.")]
+        [StringLength(10, MinimumLength = 6, ErrorMessage = "Username must be between {2} and {1}.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least {2} characters.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
+        [Display(Name = "Email address")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
+        public Gender Gender { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
+        [MustBeTrue]
+        public bool PrivacyPolicy { get; set; }
     }
 }
