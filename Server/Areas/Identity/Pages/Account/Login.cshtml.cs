95b1064d5464b8c366dc67992807e951696c2ca9
HMZ <hamzi1995@gmail.com>
2020-10-12 22:05:27 +0300

    Update login page

--- Server/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Server/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,8 +10,29 @@ namespace MVCBlazorChatApp.Server.Areas.Identity.Pages.Account
 {
     public class LoginModel : PageModel
     {
+        public LoginDto LoginDto { get; set; }
+
         public void OnGet()
         {
         }
+
+        public void OnPost()
+        {
+
+        }
+    }
+
+    public class LoginDto
+    {
+        [Required]
+        [Display(Name = "Username or email")]
+        public string UsernameOrEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
     }
 }
