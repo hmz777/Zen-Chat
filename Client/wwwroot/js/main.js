ca508077b20a8abd49e4e75831a79283de9797ab
HMZ <hamzi1995@gmail.com>
2020-10-13 21:15:53 +0300

    Update main.js

--- Client/wwwroot/js/main.js
+++ Client/wwwroot/js/main.js
@@ -7,13 +7,17 @@
         }
     });
 
-    $.validator.addMethod("mustbetrue",
-        function (value, element, parameters) {
-            return element.checked;
+    if ($.validator != null) {
+
+        $.validator.addMethod("mustbetrue",
+            function (value, element, parameters) {
+                return element.checked;
+            });
+
+        $.validator.unobtrusive.adapters.add("mustbetrue", [], function (options) {
+            options.rules.mustbetrue = {};
+            options.messages["mustbetrue"] = options.message;
         });
 
-    $.validator.unobtrusive.adapters.add("mustbetrue", [], function (options) {
-        options.rules.mustbetrue = {};
-        options.messages["mustbetrue"] = options.message;
-    });
+    }
 });
