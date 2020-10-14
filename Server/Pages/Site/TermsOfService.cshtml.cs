7945eaf1e8534601c43cc4406066699ba3cb4d5f
HMZ <hamzi1995@gmail.com>
2020-10-14 03:53:53 +0300

    Add site info pages

---
+++ Server/Pages/Site/TermsOfService.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MVCBlazorChatApp.Server.Pages.Site
+{
+    public class TermsOfServiceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
