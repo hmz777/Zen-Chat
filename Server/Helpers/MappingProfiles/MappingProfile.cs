46bc2976acef39a8dc4e16f96281800e45f1a06b
HMZ <hamzi1995@gmail.com>
2020-10-16 02:19:22 +0300

    Add and configure auto mapper

---
+++ Server/Helpers/MappingProfiles/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MVCBlazorChatApp.Server.Areas.Identity.Pages.Account;
+using MVCBlazorChatApp.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCBlazorChatApp.Server.Helpers.MappingProfiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<ApplicationUser, RegisterDTO>().ReverseMap();
+        }
+    }
+}
