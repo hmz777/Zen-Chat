9a227924b9fb79ed8a978a6d43a16cbe4349b1cb
HMZ <hamzi1995@gmail.com>
2020-10-15 21:15:37 +0300

    Add logout page

---
+++ Server/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MVCBlazorChatApp.Server.Models;
+
+namespace MVCBlazorChatApp.Server.Areas.Identity.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public LogoutModel(SignInManager<ApplicationUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnPost(string returnUrl = null)
+        {
+            await signInManager.SignOutAsync();
+
+            if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                return LocalRedirect(returnUrl);
+
+            else
+                return RedirectToPage();
+        }
+    }
+}
