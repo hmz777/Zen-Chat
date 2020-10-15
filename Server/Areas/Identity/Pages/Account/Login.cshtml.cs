cb8e9841ae08e7f8fa603647e442ae22b5fec267
HMZ <hamzi1995@gmail.com>
2020-10-16 02:18:24 +0300

    Add login logic and update login page

--- Server/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Server/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -2,23 +2,78 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MVCBlazorChatApp.Server.Helpers.StatusMessages;
+using MVCBlazorChatApp.Server.Models;
 
 namespace MVCBlazorChatApp.Server.Areas.Identity.Pages.Account
 {
     public class LoginModel : PageModel
     {
-        public LoginDto LoginDto { get; set; }
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
 
-        public void OnGet()
+        public LoginModel(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
         {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
         }
 
-        public void OnPost()
-        {
+        public LoginDto LoginDto { get; set; }
 
+        public async Task<IActionResult> OnPost(LoginDto loginDto, string ReturnUrl)
+        {
+            if (!ModelState.IsValid)
+                BadRequest(new StatusMessage { MessageStatus = MessageStatus.Failure, Message = "Invalid login." });
+
+            string UsernameOrEmail = null;
+            if (loginDto.UsernameOrEmail.Contains('@'))
+            {
+                try
+                {
+                    _ = new MailAddress(UsernameOrEmail);
+
+                    ApplicationUser user = await userManager.FindByEmailAsync(UsernameOrEmail);
+
+                    if (user != null)
+                    {
+                        //Email
+                        UsernameOrEmail = user.UserName;
+                    }
+                }
+                catch
+                {
+                    return BadRequest(new StatusMessage { MessageStatus = MessageStatus.Failure, Message = "Invalid email." });
+                }
+            }
+            else
+            {
+                //Username
+                UsernameOrEmail = loginDto.UsernameOrEmail;
+            }
+
+            var signInResult = await signInManager.PasswordSignInAsync(UsernameOrEmail, loginDto.Password, loginDto.RememberMe, true);
+
+            if (signInResult.Succeeded)
+            {
+                return new OkObjectResult(new StatusMessage { MessageStatus = MessageStatus.Success, Message = "Sign in successful." });
+            }
+            else if (signInResult.IsNotAllowed)
+            {
+                return new OkObjectResult(new StatusMessage { MessageStatus = MessageStatus.Failure, Message = "Sign in not allowed, account needs activation." });
+            }
+            else if (signInResult.IsLockedOut)
+            {
+                return new OkObjectResult(new StatusMessage { MessageStatus = MessageStatus.Failure, Message = "Too many sign in attempts, user locked out." });
+            }
+            else
+            {
+                return new OkObjectResult(new StatusMessage { MessageStatus = MessageStatus.Failure, Message = "Invalid username or password." });
+            }
         }
     }
 
