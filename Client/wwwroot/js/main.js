7bcbb16c1f3dc3374d9ea328b2a327b68bc02136
HMZ <hamzi1995@gmail.com>
2020-10-16 13:57:34 +0300

    Add ajax unobtrusive callbacks

--- Client/wwwroot/js/main.js
+++ Client/wwwroot/js/main.js
@@ -1,4 +1,6 @@
-﻿$(document).ready(function () {
+﻿var notyf;
+
+$(document).ready(function () {
     $(document).click(function (e) {
         $(".input-wrapper--text").removeClass("focused");
         var target = $(e.target);
@@ -18,6 +20,74 @@
             options.rules.mustbetrue = {};
             options.messages["mustbetrue"] = options.message;
         });
-
     }
 });
+
+function ToggleSubmitLoader(button) {
+    button.toggleClass("on");
+    button.toggleClass("disabled");
+
+    if (button.prop("disabled")) {
+        $("input:submit, button:submit").prop("disabled", false);
+    }
+    else {
+        $("input:submit, button:submit").prop("disabled", true);
+    }
+}
+
+function ClearForm(form) {
+    form.trigger('reset');
+}
+
+function ShowNotification(message, type, duration, position, dismissible) {
+    if (notyf == null)
+        notyf = new Notyf({ types: [{ type: "warning", background: 'darkorange' }] });
+
+    switch (type) {
+        case "Success":
+            notyf.success({
+                duration: duration,
+                position: position,
+                dismissible: dismissible,
+                message: message
+            });
+            break;
+        case "Failure":
+            notyf.error({
+                duration: duration,
+                position: position,
+                dismissible: dismissible,
+                message: message
+            });
+            break;
+        case "warning":
+            notyf.open({
+                type: "warning",
+                duration: duration,
+                position: position,
+                dismissible: dismissible,
+                message: message
+            });
+            break;
+        default:
+            break;
+    }
+}
+
+function FormBeforeSubmit() {
+    let button = $(this).find("button:submit");
+    ToggleSubmitLoader(button);
+}
+
+function FormAfterSubmit(response) {
+    let form = $(this);
+    let SubmitButton = form.find('button:submit');
+
+    ClearForm(form);
+    ToggleSubmitLoader(SubmitButton);
+    ShowNotification(response.message, response.status, 7000, { x: 'center', y: 'top' }, true);
+
+    if (response.link != null) {
+        location.href = link;
+    }
+}
