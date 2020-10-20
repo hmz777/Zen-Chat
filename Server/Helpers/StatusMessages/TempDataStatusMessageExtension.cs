ea5cbfd674469fb1768f1e5fb2fd2f47d6d1a779
HMZ <hamzi1995@gmail.com>
2020-10-20 17:08:20 +0300

    Add tempdata extension to get and create status messages

---
+++ Server/Helpers/StatusMessages/TempDataStatusMessageExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MVCBlazorChatApp.Server.Helpers.StatusMessages
+{
+    public static class TempDataStatusMessageExtension
+    {
+        public static void Put<T>(this ITempDataDictionary tempData, string key, T value) where T : class
+        {
+            tempData[key] = JsonSerializer.Serialize<T>(value);
+        }
+
+        public static T Get<T>(this ITempDataDictionary tempData, string key) where T : class
+        {
+            object val;
+
+            tempData.TryGetValue(key, out val);
+
+            return val == null ? null : JsonSerializer.Deserialize<T>((string)val);
+        }
+    }
+}
