790896a0b7c140bb8077842aab5b014536a75b91
HMZ <hamzi1995@gmail.com>
2020-10-22 04:57:45 +0300

    Add notyf and client.js to wasm app

---
+++ Client/wwwroot/js/client.js
@@ -0,0 +1,62 @@
+var notyf;
+
+$(document).ready(function () {
+    $(document).click(function (e) {
+        $(".input-wrapper--text").removeClass("focused");
+        var target = $(e.target);
+        if (target.parents(".input-wrapper--text").length > 0) {
+            target.parents(".input-wrapper--text").eq(0).addClass("focused");
+        }
+    });
+});
+
+function ShowNotification(message, type, duration = 5000, position = { x: 'center', y: 'top' }, dismissible = true) {
+    if (notyf == null)
+        notyf = new Notyf({ types: [{ type: "warning", background: 'darkorange' }] });
+
+    let ErrorMessage = "";
+    if (Array.isArray(message)) {
+        message.forEach(function (val, index, array) {
+            ErrorMessage += `- ${val} <br />`;
+        });
+    }
+    else
+        ErrorMessage = message;
+
+    switch (type) {
+        case 1:
+            notyf.success({
+                duration: duration,
+                position: position,
+                dismissible: dismissible,
+                message: ErrorMessage,
+                className: 'toast-custom-notyf',
+            });
+            break;
+        case 0:
+            notyf.error({
+                duration: duration,
+                position: position,
+                dismissible: dismissible,
+                message: ErrorMessage,
+                className: 'toast-custom-notyf',
+            });
+            break;
+        case 2:
+            notyf.open({
+                type: "warning",
+                duration: duration,
+                position: position,
+                dismissible: dismissible,
+                message: ErrorMessage,
+                className: 'toast-custom-notyf',
+            });
+            break;
+        default:
+            break;
+    }
+}
+
+function FocusElement(selector) {
+    document.querySelector(selector).focus();
+}
