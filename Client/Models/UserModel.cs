8aea5bbc994375999f239b1a78ef1b906f2e2cea
HMZ <hamzi1995@gmail.com>
2020-10-26 01:14:48 +0300

    Rename UsernameModel to UserModel

--- Client/Models/UserModel.cs
+++ Client/Models/UserModel.cs
@@ -1,11 +1,21 @@
+using MVCBlazorChatApp.Server.Helpers;
 using System.ComponentModel.DataAnnotations;
 
 namespace MVCBlazorChatApp.Client.Models
 {
-    public class UsernameModel
+    public class UserModel
     {
+        public UserModel()
+        {
+            Color = HelperMethods.GenerateRandomColor();
+        }
+
+        public string Color { get; set; }
+
         [Required(ErrorMessage = "Please supply a username.")]
         [StringLength(15, MinimumLength = 1, ErrorMessage = "Username length must be between {2} and {1}.")]
         public string Username { get; set; }
+
+        public string Room { get; set; }
     }
 }
