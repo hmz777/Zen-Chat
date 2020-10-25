76f75c371c6bca5ca2a24bb2010e3222f0304a6d
HMZ <hamzi1995@gmail.com>
2020-10-26 01:14:01 +0300

    Add SignalR hub

--- Server/Hubs/ChatHub.cs
+++ Server/Hubs/ChatHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using MVCBlazorChatApp.Client.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,27 +11,34 @@ namespace MVCBlazorChatApp.Server.Hubs
     {
         public async override Task OnConnectedAsync()
         {
-
         }
 
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(UserModel User, string Message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", User, Message);
         }
 
-        public async Task SendMessage(string group, string message, int messageType)
+        public async Task SendGroupMessage(UserModel User, string message)
         {
-            await Clients.Group(group).SendAsync("ReceiveNotification", message, messageType);
+            await Clients.Group(User.Room).SendAsync("ReceiveNotification", User, message);
         }
 
-        public async Task AddToGroup(string groupName)
+        public async Task<bool> AddToGroup(UserModel User, string GroupName)
         {
-            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await Groups.AddToGroupAsync(Context.ConnectionId, GroupName);
+
+            await SendGroupMessage(User, "User connected...");
+
+            return true;
         }
 
-        public async Task RemoveFromGroup(string groupName)
+        public async Task<bool> RemoveFromGroup(UserModel User, string GroupName)
         {
-            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, GroupName);
+
+            await SendGroupMessage(User, "User disconnected...");
+
+            return true;
         }
     }
 }
