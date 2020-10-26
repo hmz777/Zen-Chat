bc3989120ec2d44b922705cbd3c09d489918e55c
HMZ <hamzi1995@gmail.com>
2020-10-26 21:46:57 +0300

    Move MessageStatus to shared project

--- Shared/Models/MessageStatus.cs
+++ Shared/Models/MessageStatus.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MVCBlazorChatApp.Server.Helpers.StatusMessages
+namespace MVCBlazorChatApp.Shared.Models
 {
     public enum MessageStatus : int
     {
         Success = 1,
         Failure = 0,
-        Warning = 2
+        Warning = 2,
+        Information = 3
     }
 }
