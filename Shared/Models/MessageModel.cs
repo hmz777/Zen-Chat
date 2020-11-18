8d899fa76361d64c30027e9c6b0bf8e5f08869d0
HMZ <hamzi1995@gmail.com>
2020-11-18 12:33:30 +0200

    Move MessageModel class to the Shared project for usability between projects.

--- Shared/Models/MessageModel.cs
+++ Shared/Models/MessageModel.cs
@@ -1,13 +1,15 @@
 using MessagePack;
+using MVCBlazorChatApp.Shared.Models;
 using System.ComponentModel.DataAnnotations;
-using KeyAttribute = MessagePack.KeyAttribute;
 
-namespace MVCBlazorChatApp.Client.Models
+namespace MVCBlazorChatApp.Shared.Models
 {
     [MessagePackObject(keyAsPropertyName: true)]
     public class MessageModel
     {
         [Required(ErrorMessage = "Message can not be empty!")]
         public string Message { get; set; } = string.Empty;
+
+        public MessageStatus MessageStatus { get; set; }
     }
 }
