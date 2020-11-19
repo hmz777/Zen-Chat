232bf7e2a8de22e3e8f3b96eb9d45abc8855540d
HMZ <hamzi1995@gmail.com>
2020-11-19 17:06:37 +0200

    Update MessageModel.cs

--- Shared/Models/MessageModel.cs
+++ Shared/Models/MessageModel.cs
@@ -7,7 +7,7 @@ namespace MVCBlazorChatApp.Shared.Models
     [MessagePackObject(keyAsPropertyName: true)]
     public class MessageModel
     {
-        [Required(ErrorMessage = "Message can not be empty!")]
+        [Required(ErrorMessage = "Message can not be empty.")]
         public string Message { get; set; } = string.Empty;
 
         public MessageStatus MessageStatus { get; set; }
